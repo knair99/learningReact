19b8c267cbf9b33b9de9d76fdee0c063fa8dd5a4
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2017-11-21 00:26:55 -0800

    6. State - how to define and manage a changing state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
-  render(){
-    return <h1> {this.props.text}</h1>
+  
+  //Fjrst define a constructor
+  constructor(){
+    super(); //Binds 'this' to current object
+
+    //Next, create object to hold state
+    this.state = {
+      text: "This is state text"
+    }
   }
-}
 
-App.propTypes = {
-  text: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  age: 23
+    //Create a method to manage state
+    update (event){
+      //Define object that goes into managing state
+      this.setState({
+          text: event.target.value
+        });
+    }
+  
+  render(){
+    //Introduce an input so we can change state dynamically using update method
+    return( 
+      <div>
+          <input type="text" onChange={this.update.bind(this)}/>
+          <h1> {this.state.text} </h1>
+      </div>
+      );
+  }
 }
 
 export default App;
