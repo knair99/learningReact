aaf3dadc6992fb3d3188ec7d0a9b3c6727679af5
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2017-11-22 01:27:48 -0800

    9. Defining a state in a class component (ctor, this.state)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,8 +12,17 @@ const Button1 = (props) => <button> 7 </button>;
 
 //class component
 class Button2 extends React.Component{
+  constructor(props){
+    super(props); //This basically binds the 'this' to this component
+    
+    //Now, define the state object for this component
+    this.state = {
+      counter : 0
+    };
+  }
+
   render(){
-    return <button> 8 </button>;
+    return <button> {this.state.counter} </button>;
   }
 }
 
