e603a7ce530c8c31aa269290fc66defaf631d508
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2017-11-22 02:07:20 -0800

    Communicating between siblings through parent components (I/O)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
-  render(){
-    return <Button2/>;
-  }
-}
-
-//Function component
-const Button1 = (props) => <button> 7 </button>;
-
-//class component
-class Button2 extends React.Component{
-  constructor(props){
+    //Since both components (B1 and B2 need to communicate -define state here)
+    constructor(props){
     super(props); //This basically binds the 'this' to this component
     
     //Now, define the state object for this component
@@ -21,19 +12,34 @@ class Button2 extends React.Component{
     };
   }
 
-  //function that handles component's onClick events
-  handleClick = () => {
-    //All changes to state must be handled via setState method call
-    //This function is asynchronous = reading and writing counter can cause race condition
-    this.setState( (prevState) => { //Get the previous state by specifying a callback, which gets previous state
-      return {
-        counter: prevState.counter + 1 //update new counter using counter value from previous state
-      };
+  incrementCounter = () => {
+    this.setState( (prevState) => {
+      return {counter: prevState.counter + 1};
     });
   }
 
   render(){
-    return <button onClick={this.handleClick}> {this.state.counter} </button>;
+    //We can pass a reference to anything from the parent to the child component 
+    //This can just be data, which will be available in the child's prop - like Div component
+    //This can also be a callback, which will be available as a prop in the child - like button component
+    return (
+      <div>
+        <DivComponent counter_variable = {this.state.counter}/>
+        <ButtonComponent onClickFunction={this.incrementCounter}/>
+      </div>);
+  }
+}
+
+//Function component
+const DivComponent = (props) => <div> {props.counter_variable} </div>;
+
+//class component
+class ButtonComponent extends React.Component{
+   //function that handles component's onClick events
+
+  render(){
+    //The passed callback is available as a props member now
+    return <button onClick={this.props.onClickFunction}> +1 </button>;
   }
 }
 
